a8ab74923fc40cb41ec2070ae1ab868222384d8e
Mathieu-Asselbergs <65446703+Mathieu-Asselbergs@users.noreply.github.com>
2025-02-04 01:39:44 +0100

    Copied and adapted a prefab project
    Adapted the blinky blocking display project on the GitHub page for the `microbit` crate to show an endless stream of expanding rings.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
+use defmt_rtt as _;
 use panic_halt as _;
 
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let Some(board) = Board::take() else {
+        panic!("Couldn't take ownership of the board!");
+    };
+
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+
+    let mut frame = [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+    ];
+
+    loop {
+        display.show(&mut timer, frame, 1000);
+        
+        for row in &mut frame {
+            for col in row {
+                *col = 1 - *col;
+            }
+        }
+    }
 }
