c4b099bf7b8c24a41a5813f3bfe8fafbcbb7a91c
Mathieu-Asselbergs <65446703+Mathieu-Asselbergs@users.noreply.github.com>
2025-02-04 02:45:14 +0100

    Implemented Conway's game of life
    Ref:
    https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,65 @@ use defmt_rtt as _;
 use panic_halt as _;
 
 use cortex_m_rt::entry;
-use embedded_hal::delay::DelayNs;
 use microbit::{board::Board, display::blocking::Display, hal::Timer};
+
+
+fn random_automaton() -> [[u8; 5]; 5] {
+    static mut SEED: u16 = 39333;
+    let mut result = [[0; 5]; 5];
+
+    unsafe {
+        let square = SEED as u32 * SEED as u32;
+        SEED = ((square << 8) >> 16) as u16;
+
+        let mut mask = 1;
+        for r in 0..5 {
+            for c in 0..5 {
+                result[r][c] = (square & mask) as u8;
+                mask <<= 1;
+            }
+        }
+        
+        result
+    }
+}
+
+fn update_automata<F>(automata: &mut [[u8; 5]; 5], transition_function: F)
+where
+    F: Fn(u8, [u8; 8]) -> u8
+{
+    let dummy = *automata;
+
+    for row in 0..5 {
+        for col in 0..5 {
+            automata[row][col] = transition_function(
+                dummy[row][col],
+                [
+                    dummy[(row + 4) % 5][(col + 4) % 5],     // Top left
+                    dummy[(row + 4) % 5][col],               // Top middle
+                    dummy[(row + 4) % 5][(col + 1) % 5],     // Top right
+                    
+                    dummy[row][(col + 4) % 5],               // Middle left
+                    dummy[row][(col + 1) % 5],               // Middle right
+                    
+                    dummy[(row + 1) % 5][(col + 4) % 5],     // Bottom left
+                    dummy[(row + 1) % 5][col],               // Bottom middle
+                    dummy[(row + 1) % 5][(col + 1) % 5],     // Bottom right
+                ]
+            );
+        }
+    }
+}
+
+fn conway_transitions(center_cell: u8, neighbors: [u8; 8]) -> u8 {
+    let live_neighbor_count = neighbors.iter().filter(|n| **n != 0).count();
+
+    match (center_cell, live_neighbor_count) {
+        (0, 3) => 1,
+        (_, 2..=3) => 1,
+        _ => 0,
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -16,22 +73,11 @@ fn main() -> ! {
 
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-
-    let mut frame = [
-        [1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1],
-    ];
+    let mut automata = random_automaton();
 
     loop {
-        display.show(&mut timer, frame, 1000);
-        
-        for row in &mut frame {
-            for col in row {
-                *col = 1 - *col;
-            }
-        }
+        display.show(&mut timer, automata, 1000);
+
+        update_automata(&mut automata, conway_transitions);
     }
 }
